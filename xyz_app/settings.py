30278cae1c139e199dfe5e440eb6eb2b05aa435c
ynj0906 <n.yasuhito@gmail.com>
2018-08-18 01:14:42 +0900

    訂正2回目

--- xyz_app/settings.py
+++ xyz_app/settings.py
@@ -118,4 +118,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- xyz_app/urls.py
+++ xyz_app/urls.py
@@ -20,5 +20,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^item/', include('item.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^dog-cat-beef', include('item.urls')),
 ]
