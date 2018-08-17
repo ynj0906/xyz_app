4a8c0e9a48618d9548c9f862fc9e85207d1e2074
ynj0906 <n.yasuhito@gmail.com>
2018-08-17 22:55:47 +0900

    url.py訂正1

--- xyz_app/urls.py
+++ xyz_app/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^item/', include('item.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^dog-cat-beef', include('item.urls')),
 ]
