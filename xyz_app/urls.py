fc47d136abba25c43def07701534509febe45ce1
ynj0906 <n.yasuhito@gmail.com>
2018-08-19 03:44:57 +0900

    どや？

--- xyz_app/urls.py
+++ xyz_app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^item/', include('item.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^item/', include('item.urls')),
 ]
